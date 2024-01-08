6c10ff2844baae0795c6d36b39e3502639e75dfb
RLUCAS <lucas.reboulet@ynov.com>
2024-01-08 21:12:26 +0100

    add route '/inscription'

--- src/index.php
+++ src/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "test";
+require_once __DIR__ . '/controllers/registerController.php';
+
+$routes = [
+    '/inscription' => ['controller' => 'registerController', 'method' => 'register'],
+];
+
+$request = $_SERVER['REQUEST_URI'];
+if (array_key_exists($request, $routes)) {
+    $controllerName = $routes[$request]['controller'];
+    $methodName = $routes[$request]['method'];
+
+    $controller = new $controllerName();
+    $controller->$methodName();
+}
