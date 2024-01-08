d27f2cc4952472d7af2cff7f0bd70b5b1f670d02
SlayzerZ <ibrahimallari84@gmail.com>
2024-01-08 17:59:17 +0100

    config

---
+++ src/_config.php
@@ -0,0 +1,35 @@
+<?php 
+ini_set('display_errors','on');
+error_reporting(E_ALL);
+
+class MyAutoLoad{
+    public static function start(){
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+
+        $host = $_SERVER['HTTP_HOST'];
+        $root = $_SERVER['DOCUMENT_ROOT'];
+
+        define('HOST', 'http://'.$host);
+        define('ROOT', $root);
+        define('CONTROLLER', ROOT.'/controllers/');
+        define('VIEW', ROOT.'/views/');
+        define('MODEL', ROOT.'/models/');
+        define('ASSET', HOST.'/assets/');
+        define('CSS', ASSET.'css/');
+        define('PICTURE', ASSET.'pictures/');
+        define('CARS', PICTURE.'cars/');
+        define('CLASSE', ROOT.'/class/');
+        define('VENDOR', ROOT.'/vendor/');
+    }
+    public static function autoload($class){
+        if(file_exists(MODEL.$class.'.php')){
+            include_once(MODEL.$class.'.php');
+        } else if (file_exists(CLASSE.$class.'.php')){
+            include_once(CLASSE.$class.'.php');
+        } else if (file_exists(CONTROLLER.$class.'.php')){
+            include_once(CONTROLLER.$class.'.php');
+        };
+        require (VENDOR.'autoload.php');
+    }
+}
+?>
--- src/index.php
+++ src/index.php
@@ -1,3 +1,9 @@
 <?php
+include_once('_config.php');
+date_default_timezone_set('Europe/Paris');
+MyAutoLoad::start();
+
 
 echo "test";
+
+?>
