21a58d602fd42bafd2921268aefd8b120effd0c1
RLUCAS <lucas.reboulet@ynov.com>
2024-01-08 21:22:05 +0100

    add route '/'

--- src/index.php
+++ src/index.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once __DIR__ . '/controllers/registerController.php';
+require_once __DIR__ . '/controllers/homeContoller.php';
 
 $routes = [
+    '/' => ['controller' => 'homeController', 'method' => 'home'],
     '/inscription' => ['controller' => 'registerController', 'method' => 'register'],
 ];
 
