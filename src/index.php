30793fe5b0bb0ad8646572e746c2b70956035b23
RLUCAS <lucas.reboulet@ynov.com>
2024-01-08 21:29:31 +0100

    add route '/connexion'

--- src/index.php
+++ src/index.php
@@ -1,12 +1,14 @@
 <?php
 
-require_once __DIR__ . '/controllers/registerController.php';
 require_once __DIR__ . '/controllers/homeContoller.php';
 require_once __DIR__ . '/controllers/summaryController.php';
+require_once __DIR__ . '/controllers/loginController.php';
+require_once __DIR__ . '/controllers/registerController.php';
 
 $routes = [
     '/' => ['controller' => 'homeController', 'method' => 'home'],
     '/resume' => ['controller' => 'summaryController', 'method' => 'summary'],
+    '/connexion' => ['controller' => 'loginController', 'method' => 'login'],
     '/inscription' => ['controller' => 'registerController', 'method' => 'register'],
 ];
 
