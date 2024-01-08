9e4e678357c19e8a91dc5e1c5ac732c0e4baebfa
RLUCAS <lucas.reboulet@ynov.com>
2024-01-08 21:33:16 +0100

    add route '/panelAdmin'

--- src/index.php
+++ src/index.php
@@ -4,12 +4,14 @@ require_once __DIR__ . '/controllers/homeContoller.php';
 require_once __DIR__ . '/controllers/summaryController.php';
 require_once __DIR__ . '/controllers/loginController.php';
 require_once __DIR__ . '/controllers/registerController.php';
+require_once __DIR__ . '/controllers/dashboardController.php';
 
 $routes = [
     '/' => ['controller' => 'homeController', 'method' => 'home'],
     '/resume' => ['controller' => 'summaryController', 'method' => 'summary'],
     '/connexion' => ['controller' => 'loginController', 'method' => 'login'],
     '/inscription' => ['controller' => 'registerController', 'method' => 'register'],
+    '/panelAdmin' => ['controller' => 'dashboardController', 'method' => 'dashboard'],
 ];
 
 $request = $_SERVER['REQUEST_URI'];
