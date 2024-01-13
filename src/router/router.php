06cbbddebb683b3ed4de25d8edab9862f6607b6a
SlayzerZ <ibrahimallari84@gmail.com>
2024-01-13 16:30:29 +0100

    aWork on router

--- src/router/router.php
+++ src/router/router.php
@@ -14,37 +14,6 @@ class router{
         'deconnexion' => ['controller' => 'logOutController', 'method' => 'logOut'],
         'mWork' => ['controller' => 'summaryController', 'method' => 'modify'],
         'getImage' => ['controller' => 'summaryController', 'method' => 'getImage'],
-    ];
-    public function __construct($request)
-    {
-        $this->request = $request;
-    }
-    public function renderController(){
-        $request = $this->request;
-        if(key_exists($request, $this->routes)){
-            $controller = $this->routes[$request]['controller'];
-            $method = $this->routes[$request]['method'];
-            $currentController = new $controller();
-            $currentController->$method();
-        } else {
-            // include(CONTROLLER.'404Controller.php');
-        }
-    }
-}
-?><?php 
-class router{
-    private $request;
-    private $routes = [
-        'home' => ['controller' => 'homeController', 'method' => 'home'],
-        'resume' => ['controller' => 'summaryController', 'method' => 'summary'],
-        'connexion' => ['controller' => 'loginController', 'method' => 'login'],
-        'inscription' => ['controller' => 'registerController', 'method' => 'register'],
-        'panelAdmin' => ['controller' => 'dashboardController', 'method' => 'dashboard'],
-        'oeuvres' => ['controller' => 'worksController', 'method' => 'works'],
-        'profil' => ['controller' => 'profilController', 'method' => 'profil'],
-        'recommande' => ['controller' => 'recommendedController', 'method' => 'recommended'],
-        'solunasList' => ['controller' => 'solunaslistController', 'method' => 'solunaslist'],
-        'deconnexion' => ['controller' => 'logOutController', 'method' => 'logOut'],
         'aWork' => ['controller' => 'worksController', 'method' => 'add'],
     ];
     public function __construct($request)
