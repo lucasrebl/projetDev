ad898289aef80d455bdd3b7c90499538427ddbc7
D0ria <doriaremadna@gmail.com>
2024-01-19 12:11:27 +0100

    add:responsive navbar

--- src/static/js/home.js
+++ src/static/js/home.js
@@ -1,16 +1,31 @@
 var content = document.getElementsByTagName("body")[0];
 var darkMode = document.getElementById("dark-change");
 var modeImage = document.getElementById("modeImage");
+const actionBtn = document.querySelector('.action-btn');
+const actionBtnIcon = document.querySelector('.action-btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+actionBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    actionBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+}
+
 darkMode.addEventListener('click', function () {
-darkMode.classList.toggle('active');
-content.classList.toggle('night');
-toggleImage();
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night');
+    toggleImage();
 });
 
 function toggleImage() {
-if (darkMode.classList.contains('active')) {
-modeImage.src = '/../../static/asset/darkmode.png';
-} else {
-modeImage.src = '/../../static/asset/lightmode.png';
+    if (darkMode.classList.contains('active')) {
+        modeImage.src = '/../../static/asset/darkmode.png';
+    } else {
+        modeImage.src = '/../../static/asset/lightmode.png';
+    }
 }
-}
+
+
