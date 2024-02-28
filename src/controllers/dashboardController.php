a55ba5206e608d567743ccd715f03c94b3f68ec0
RLUCAS <lucas.reboulet@ynov.com>
2024-02-28 12:03:57 +0100

    crud works finish

--- src/controllers/dashboardController.php
+++ src/controllers/dashboardController.php
@@ -29,8 +29,11 @@ class dashboardController
             include __DIR__ . '/../models/dashboardModel.php';
             $this->crudUser();
             $result = $this->readUser();
+            $this->crudOeuvres();
+            $result2 = $this->readOeuvres();
             echo $this->twig->render('dashboard/dashboard.html.twig', [
-                'userDetails' => $result
+                'userDetails' => $result,
+                'oeuvresDetails' => $result2
             ]);
         }
     }
@@ -67,7 +70,6 @@ class dashboardController
                 echo "Utilisateur ajouté avec succès!";
             }
         }
-        
         // condittion update pictures user
         else if (isset($_POST['update2'])) {
             $user_username_select = $_POST['usernameSelect'];
@@ -96,5 +98,73 @@ class dashboardController
             }
         }
         return $result;
+    }
+
+    public function crudOeuvres()
+    {
+        // conditions delete oeuvres
+        if (isset($_POST['delete2'])) {
+            $oeuvres_id_delete = $_POST['id'];
+            deleteOeuvres($oeuvres_id_delete);
+        }
+        // conditions update oeuvres
+        else if (isset($_POST['update3'])) {
+            $oeuvres_id_select = $_POST['idSelect'];
+            $oeuvres_nameWorks_update = $_POST['nameWorks'];
+            $oeuvres_status_update = $_POST['status'];
+            $oeuvres_summary_update = $_POST['summary'];
+            $oeuvres_numberOfEpisodes_update = $_POST['numberOfEpisodes'];
+            $oeuvres_numberOfSeason_update = $_POST['numberOfSeason'];
+            $oeuvres_numberOfTome_update = $_POST['numberOfTome'];
+            $oeuvres_image_update = $_FILES['image'];
+            updateOeuvres($oeuvres_id_select, $oeuvres_nameWorks_update, $oeuvres_status_update, $oeuvres_summary_update, $oeuvres_numberOfEpisodes_update, $oeuvres_numberOfSeason_update, $oeuvres_numberOfTome_update);
+        }
+        // conditions add oeuvres
+        if (isset($_POST['submit2'])) {
+            $oeuvres_nameWorks_add = $_POST['nameWorks'];
+            $oeuvres_status_add = $_POST['status'];
+            $oeuvres_summary_add = $_POST['summary'];
+            $oeuvres_numberOfEpisodes_add = $_POST['numberOfEpisodes'];
+            $oeuvres_numberOfSeason_add = $_POST['numberOfSeason'];
+            $oeuvres_numberOfTome_add = $_POST['numberOfTome'];
+            if (empty($oeuvres_nameWorks_add) || empty($oeuvres_status_add) || empty($oeuvres_summary_add) || empty($oeuvres_numberOfEpisodes_add) || empty($oeuvres_numberOfSeason_add) || empty($oeuvres_numberOfTome_add)) {
+                echo "Tous les champs doivent être remplis.";
+            } else {
+                if (isset($_FILES["image"]) && $_FILES["image"]["error"] == UPLOAD_ERR_OK) {
+                    $oeuvres_image_add = file_get_contents($_FILES["image"]["tmp_name"]);
+                    addOeuvres($oeuvres_nameWorks_add, $oeuvres_status_add, $oeuvres_summary_add, $oeuvres_numberOfEpisodes_add, $oeuvres_numberOfSeason_add, $oeuvres_numberOfTome_add, $oeuvres_image_add);
+                } else {
+                    echo "Erreur lors du téléchargement de l'image.";
+                }
+            }
+        }
+        // condittion update pictures user
+        if (isset($_POST['update4'])) {
+            $oeuvres_id_select = $_POST['idSelect'];
+            if (isset($_FILES["image"]) && $_FILES["image"]["error"] == UPLOAD_ERR_OK) {
+                $oeuvres_image_update = file_get_contents($_FILES["image"]["tmp_name"]);
+                updateOeuvresImage($oeuvres_id_select, $oeuvres_image_update);
+            } else {
+                echo "Erreur lors du téléchargement de l'image.";
+            }
+        }
+    }
+
+    public function readOeuvres()
+    {
+        $dsn = new PDO("mysql:host=mysql;dbname=my_database", "my_user", "my_password");
+        $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $dsn->prepare("SELECT * FROM works");
+        $stmt->execute();
+        $result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($result2 as &$user) {
+            if ($user['image'] !== null) {
+                $user['image'] = base64_encode($user['image']);
+            } else {
+                $user['image'] = '';
+            }
+        }
+        return $result2;
     }
 }
--- src/models/dashboardModel.php
+++ src/models/dashboardModel.php
@@ -114,12 +114,153 @@ if (!function_exists('updateUserPictures')) {
         try {
             $dsn = new PDO("mysql:host=mysql;dbname=my_database", "my_user", "my_password");
             $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
+
             $stmt = $dsn->prepare("UPDATE user SET pictures = :pictures WHERE username = :usernameSelect");
             $stmt->bindParam(':usernameSelect', $user_username_select);
             $stmt->bindParam(':pictures', $user_pictures_update, PDO::PARAM_LOB);
             if ($stmt->execute()) {
                 echo "update réussis";
+            } else {
+                echo "echec update";
+            }
+        } catch (PDOException $e) {
+            $error = "Error: " . $e->getMessage();
+            echo $error;
+        }
+    }
+}
+
+if (!function_exists('deleteOeuvres')) {
+    function deleteOeuvres($oeuvres_id_delete)
+    {
+        try {
+            $dsn = new PDO("mysql:host=mysql;dbname=my_database", "my_user", "my_password");
+            $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $dsn->prepare("DELETE FROM works WHERE idWorks = :id");
+            $stmt->bindParam(':id', $oeuvres_id_delete);
+            if ($stmt->execute()) {
+                echo "suppression oeuvres réussis";
+            } else {
+                echo "echec de la suppression";
+            }
+        } catch (PDOException $e) {
+            $error = "Error: " . $e->getMessage();
+            echo $error;
+        }
+    }
+}
+
+if (!function_exists('updateOeuvres')) {
+    function updateOeuvres($oeuvres_id_select, $oeuvres_nameWorks_update, $oeuvres_status_update, $oeuvres_summary_update, $oeuvres_numberOfEpisodes_update, $oeuvres_numberOfSeason_update, $oeuvres_numberOfTome_update)
+    {
+        try {
+            $dsn = new PDO("mysql:host=mysql;dbname=my_database", "my_user", "my_password");
+            $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $setClauses = [];
+            if (!empty($oeuvres_nameWorks_update)) {
+                $setClauses[] = "nameWorks = :nameWorks";
+            }
+            if (!empty($oeuvres_status_update)) {
+                $setClauses[] = "status = :status";
+            }
+            if (!empty($oeuvres_summary_update)) {
+                $setClauses[] = "summary = :summary";
+            }
+            if (!empty($oeuvres_numberOfEpisodes_update)) {
+                $setClauses[] = "numberOfEpisodes = :numberOfEpisodes";
+            }
+            if (!empty($oeuvres_numberOfSeason_update)) {
+                $setClauses[] = "numberOfSeason = :numberOfSeason";
+            }
+            if (!empty($oeuvres_numberOfTome_update)) {
+                $setClauses[] = "numberOfTome = :numberOfTome";
+            }
+
+            if (empty($setClauses)) {
+                echo "Aucun champ à mettre à jour.";
+                return;
+            }
+
+            $query = "UPDATE works SET " . implode(', ', $setClauses) . " WHERE idWorks = :idWorks ";
+            $stmt = $dsn->prepare($query);
+
+            $stmt->bindParam(':idWorks', $oeuvres_id_select);
+            if (!empty($oeuvres_id_select)) {
+                $stmt->bindParam(':idWorks', $oeuvres_id_select);
+            }
+            if (!empty($oeuvres_nameWorks_update)) {
+                $stmt->bindParam(':nameWorks', $oeuvres_nameWorks_update);
+            }
+            if (!empty($oeuvres_status_update)) {
+                $stmt->bindParam(':status', $oeuvres_status_update);
+            }
+            if (!empty($oeuvres_summary_update)) {
+                $stmt->bindParam(':summary', $oeuvres_summary_update);
+            }
+            if (!empty($oeuvres_numberOfEpisodes_update)) {
+                $stmt->bindParam(':numberOfEpisodes', $oeuvres_numberOfEpisodes_update);
+            }
+            if (!empty($oeuvres_numberOfSeason_update)) {
+                $stmt->bindParam(':numberOfSeason', $oeuvres_numberOfSeason_update);
+            }
+            if (!empty($oeuvres_numberOfTome_update)) {
+                $stmt->bindParam(':numberOfTome', $oeuvres_numberOfTome_update);
+            }
+
+            if ($stmt->execute()) {
+                echo "Mise à jour réussie.";
+            } else {
+                echo "Échec de la mise à jour.";
+            }
+        } catch (PDOException $e) {
+            $error = "Error: " . $e->getMessage();
+            echo $error;
+        }
+    }
+}
+
+if (!function_exists('addOeuvres')) {
+    function addOeuvres($oeuvres_nameWorks_add, $oeuvres_status_add, $oeuvres_summary_add, $oeuvres_numberOfEpisodes_add, $oeuvres_numberOfSeason_add, $oeuvres_numberOfTome_add, $oeuvres_image_add)
+    {
+        try {
+            $dsn = new PDO("mysql:host=mysql;dbname=my_database", "my_user", "my_password");
+            $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $dsn->prepare("INSERT INTO works (nameWorks, status, image, summary, numberOfEpisodes, numberOfSeason, numberOfTome) VALUES (:nameWorks, :status, :image, :summary, :numberOfEpisodes, :numberOfSeason, :numberOfTome)");
+            $stmt->bindParam(':nameWorks', $oeuvres_nameWorks_add);
+            $stmt->bindParam(':status', $oeuvres_status_add);
+            $stmt->bindParam(':summary', $oeuvres_summary_add);
+            $stmt->bindParam(':numberOfEpisodes', $oeuvres_numberOfEpisodes_add);
+            $stmt->bindParam(':numberOfSeason', $oeuvres_numberOfSeason_add);
+            $stmt->bindParam(':numberOfTome', $oeuvres_numberOfTome_add);
+            $stmt->bindParam(':image', $oeuvres_image_add, PDO::PARAM_LOB);
+            if ($stmt->execute()) {
+                echo "ajout réussis";
+            } else {
+                echo "ajout non réussis";
+            }
+
+        } catch (PDOException $e) {
+            $error = "Error: " . $e->getMessage();
+            echo $error;
+        }
+    }
+}
+
+if (!function_exists('updateOeuvresImage')) {
+    function updateOeuvresImage($oeuvres_id_select, $oeuvres_image_update)
+    {
+        try {
+            $dsn = new PDO("mysql:host=mysql;dbname=my_database", "my_user", "my_password");
+            $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $dsn->prepare("UPDATE works SET image = :image WHERE idWorks = :idWorks");
+            $stmt->bindParam(':idWorks', $oeuvres_id_select);
+            $stmt->bindParam(':image', $oeuvres_image_update, PDO::PARAM_LOB);
+            if ($stmt->execute()) {
+                echo "update image réussis";
             } else {
                 echo "echec update";
             }
